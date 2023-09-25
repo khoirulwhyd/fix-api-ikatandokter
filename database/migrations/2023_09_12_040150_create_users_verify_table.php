689355ecd1cd1372d150a912995e0a5b738d64df
Sholikin <1941720140@student.polinema.ac.id>
2023-09-25 13:17:17 +0700

    update fk

--- database/migrations/2023_09_12_040150_create_users_verify_table.php
+++ database/migrations/2023_09_12_040150_create_users_verify_table.php
@@ -12,9 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users_verify', function (Blueprint $table) {
-            $table->integer('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('token');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
         
     }
